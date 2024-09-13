6aa1d315064f4977f59ca8d66857bb0dec12b1f1
Lucas Marinho <lucasmarinhobasilio@outlook.com>
2024-09-13 11:12:33 -0300

    Delete lista6 directory

--- lista6/lista6ex01.py
+++
@@ -1,9 +0,0 @@
-print("Bem vindo!")
-N = int(input("Insira um numero: "))
-if N not in range(1,11):
-  print("Tente novamente")
-else:
-  print("A tabuada de %i é: "% (N))
-  for i in range(1,11):
-    valor = i*N
-    print("%ix%i = %i" % (N,i,valor))
--- lista6/lista6ex02.py
+++
@@ -1,9 +0,0 @@
-print("Bem vindo!")
-valor_inicial = float(input("Insira o deposito inicial: "))
-taxa = float(input("Insira a taxa de juros em decimal: "))
-for i in range(1,25):
-  mes = i
-  montante = valor_inicial*((1+taxa)**mes)
-  print(f"O valor no mes {mes} é: {montante: .2f}R$")
-juros = montante - valor_inicial
-print(f"O valor dos juros é: {juros: .2f}R$")
