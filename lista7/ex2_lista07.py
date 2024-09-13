0ee3410a26ecc1bef61f6c0edcd5391bca6e3ab5
Lucas Marinho <lucasmarinhobasilio@outlook.com>
2024-09-13 11:12:46 -0300

    Delete lista7 directory

--- lista7/ex2_lista07.py
+++
@@ -1,9 +0,0 @@
-matriz = [[1,2,3], [4,5,6], [7,8,9]]
-matriz_2 = [[None for _ in range(3)] for _ in range(3)]
-
-for i in range(3):
-    
-    for j in range(3):
-        matriz_2[j][i] = matriz[i][j]
-        
-print(matriz_2)
