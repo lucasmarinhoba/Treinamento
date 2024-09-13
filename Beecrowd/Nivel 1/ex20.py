a79af62e515d4ad7f08682d17d154e499420c9ea
Lucas Marinho <lucasmarinhobasilio@outlook.com>
2024-09-13 11:52:57 -0300

    Create ex20.py

---
+++ Beecrowd/Nivel 1/ex20.py
@@ -0,0 +1,10 @@
+'''Papai Noel está brincando com seus duendes para entretê-los durante a véspera do Natal. A brincadeira consiste nos elfos escreverem números em pedaços de papel e colocarem no gorro do Papai Noel. Após todos terminarem de colocar os números Noel sorteia um papel e aquele número representa quantos "Ho" o Noel deve falar.
+
+Seu trabalho é ajudar o Papai Noel montando um problema que mostre todos os "Ho" que ele deve falar dado o número sorteado.
+'''
+num = int(input())
+matriz = ['Ho ']*num
+letra = ''*num
+for i in range(num):
+  letra += matriz[i]
+print(letra)
