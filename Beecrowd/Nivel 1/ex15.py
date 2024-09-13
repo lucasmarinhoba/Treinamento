e381eb89a4bad1f55870df05d35e099de96b61bd
Lucas Marinho <lucasmarinhobasilio@outlook.com>
2024-09-13 11:44:15 -0300

    Create ex15.py

---
+++ Beecrowd/Nivel 1/ex15.py
@@ -0,0 +1,7 @@
+'''Escreva um programa que leia um valor inteiro N (1 < N < 1000). Este N é a quantidade de linhas de saída que serão apresentadas na execução do programa.'''
+
+for i in range (1,1000):
+  a = i
+  b = i**2
+  c = i**3
+  print(a,b, c)
