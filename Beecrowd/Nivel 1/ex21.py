fd92edb0f9c9abf862f5fb710295ddbb20b80c59
Lucas Marinho <lucasmarinhobasilio@outlook.com>
2024-09-13 11:53:23 -0300

    Create ex21.py

---
+++ Beecrowd/Nivel 1/ex21.py
@@ -0,0 +1,13 @@
+'''Dois amigos pedem ao atendente de uma lanchonete propor um desafio, de modo que quem acertasse mais, não precisaria pagar a conta. Então foi proposto o seguinte: Dado o seguinte somatório abaixo, informar o resultado, com uma quantidade de termos no mesmo:
+
+S = 1 - 1 + 1 - 1 + 1 - 1 + 1 - 1 ...
+
+Escreva um programa que, dada uma quantidade de termos, informar o resultado do somatório acima.'''
+
+casos = int(input())
+for i in range(casos):
+    n = int(input())
+    if n % 2 == 0:
+        print("0")
+    else:
+        print("1")
